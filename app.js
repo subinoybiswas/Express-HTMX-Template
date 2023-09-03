db4f79ba38ccf6fbe7975e0da723fa69e57935fd
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2023-09-04 00:30:22 +0530

    Implemented Basic Structure

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const pageRoutes = require("./routes/page");
+var path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.set(express.static(path.join(__dirname, "views")));
 app.use("/page", pageRoutes);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/index.html"));
+});
+
 
 app.listen(PORT);
 console.log("Listening on ", PORT);
+console.log("http://localhost:" + PORT + "/");
--- routes/page.js
+++ routes/page.js
@@ -5,3 +5,5 @@ const pageRouter = express.Router();
 pageRouter.get("/", (req, res) => {
   res.send("Yay!");
 });
+
+module.exports = pageRouter;
